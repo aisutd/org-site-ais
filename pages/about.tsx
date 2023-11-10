88bbabd9ea9dd6cc6f02e06ae97c5fab57c88deb
RishilU <112522578+RishilU@users.noreply.github.com>
2023-11-10 12:45:15 -0600

    About us rough draft

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,116 +7,66 @@ import { maxWidth } from '@mui/system';
  */
 const spacing = 10;
 
-const storyText = [
-  ["Jeff Bezos, the founder and executive chairman of Amazon, stated in May of 2016 that society is on the cusp of entering the golden age of artificial intelligence (AI). That, over the next two decades, human society and AI will become intertwined in a way that would seem like science fiction today. That modern AI tools, such as Alexa, IBM Watson, and Google TensorFlow, only scratch the surface of what's possible.", false],
-  ["A month after Mr. Bezos' statement, the Artificial Intelligence Society (AIS) was born. In fact, our organization began under the general fascination of the impact that AI has already made on the world. Whether that be through Netflix's movie recommendations or Facebook's facial recognition software, it is incredible how ubiquitous AI has become in our daily lives. But as of today, many of the fascinating aspects of AI are largely unknown to the general public.", false],
-  ["We want to change that.", true],
-  ["Since 2019, AIS has hosted at least 9 workshops, 6 talks, and 3 iterations of HackAI, a full day of learning about the field of AI with over 400 participants. Regardless of age, major, or occupation, we believe that AI is for everyone, as it has become more common in much of our everyday activities, whether it's YouTube recommendations or facial recognition. At AIS, we hope to delve into the nuts and bolts of artificial intelligence, as well as showcasing its massive potential.", false],
-];
 
 export default function AboutPage() {
-
-  return (
-    <div>
-      <Head>
-        <title>About &ndash; AIS</title>
-        <meta
-          name="description"
-          content="A brief history of the Artficial Intelligence Society at UTD and how we've grown over the years."
-        />
-      </Head>
-      <main>
-        <Box marginTop={spacing} sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}>
-          <Box textAlign="center" sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundImage: 'url("top.png")',
-            backgroundSize: "cover",
-            width: "1440px",
-            height: "350px"
-          }}>
-            <Box>
-              <Typography marginBottom="1rem" color="white" textTransform="uppercase" 
-                          fontFamily="Avenir" fontSize="2rem" fontWeight="800" >
-                About Us
-              </Typography>
-              <Typography color="white" fontFamily="Vollkorn" fontSize="1.3rem" >
-                A brief history of the Artficial Intelligence Society at UTD and how we&apos;ve grown over the years.
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
-        <Container sx={{ marginTop: "3rem"}}>
-          <Box marginBottom={5}>
-            <Box>
-              <Typography fontSize={40} fontFamily='Bentham' marginBottom={4}  variant='h2'>
-                Our story
-              </Typography>
-              <svg width="398" height="3" viewBox="0 0 398 3" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <line x1="1.5" y1="1.5" x2="396.5" y2="1.5" stroke="url(#paint0_linear_263_1158)" strokeWidth="3" strokeLinecap="round"/>
-                <defs>
-                <linearGradient id="paint0_linear_263_1158" x1="0" y1="3" x2="398" y2="3" gradientUnits="userSpaceOnUse">
-                <stop stopColor="#24314D"/>
-                <stop offset="1" stopColor="#24314D" stopOpacity="0"/>
-                </linearGradient>
-                </defs>
-              </svg>
-            </Box>
-            <Box marginTop={spacing}>
-              {storyText.map((story, key) => {
-                return (
-                story[1] ? 
-                <Typography color="#4F4F4F" fontWeight={800} marginBottom={5} key={key} fontSize={24} fontFamily='Vollkorn'>
-                  {story[0]}
-                </Typography> : 
-                <Typography color="#4F4F4F" marginBottom={5} key={key} fontSize={24} fontFamily='Vollkorn'>
-                {story[0]}
-              </Typography>);
-              })}
-            </Box>
-          </Box>
-          <Box marginBottom={10} sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between"
-          }}>
-            <Box>
-              <Box marginBottom={5}>
-                <Typography fontFamily="Bentham" fontSize={40}>Our Founder</Typography>
-              </Box>
-              <Box sx={{
-                width: "15rem",
-                border: "2px solid black",
-                borderRadius: "5px",
-              }}>
-                <img src="images/founder.jpeg"></img>
-              </Box>
-              <Box sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-              }}>
-                <Typography fontWeight={800} fontFamily="Avenir" color="#0B132A" fontSize={24}>Brian Hoang</Typography>
-                <Typography fontFamily="Avenir" color="#666666" fontSize={24}>- Founder (SE &apos;18) -</Typography>
-              </Box>
-            </Box>
-            <Box maxWidth={"60%"}>
-              <svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M32.0833 52.5V29.1667H26.6L32.4333 17.5H15.6917L8.75 31.3833V52.5M26.25 46.6667H14.5833V32.7833L19.3083 23.3333H22.9833L17.15 35H26.25M61.25 52.5V29.1667H55.7667L61.6 17.5H44.8583L37.9167 31.3833V52.5M55.4167 46.6667H43.75V32.7833L48.475 23.3333H52.15L46.3167 35H55.4167V46.6667Z" fill="#0B132A" fillOpacity="0.8"/>
-              </svg>
-              <Typography fontFamily="Vollkorn" align='center' fontSize={24}>
-              “A month after Mr. Bezos&apos; statement, the Artificial Intelligence Society (AIS) was born. In fact, our organization began under the general fascination of the impact that AI has already made on the world. Whether that be through Netflix&apos;s movie recommendations or Facebook&apos;s facial recognition software, it is incredible how ubiquitous AI has become in our daily lives. But as of today, many of the fascinating aspects of AI are largely unknown to the general public.”
-              </Typography>
-            </Box>
-          </Box>
-        </Container>
-      </main>
-    </div>
+   return (
+     <><div className="relative w-[1440px] h-[267px] bg-[#fff8f3]">
+       <div className="relative w-[1213px] h-[259px] left-[107px]">
+         <div className="flex flex-col w-[1200px] items-center gap-[16px] absolute top-[300px] left-[13px]">
+           <div className="relative self-stretch w-full flex-[0_0_auto]" />
+         </div>
+         <div className="absolute w-[1200px] top-[89px] left-[13px] [text-shadow:0px_4px_4px_#00000040] font-h1 font-[number:var(--h1-font-weight)] text-[#3e6eff] text-[length:var(--h1-font-size)] text-center tracking-[var(--h1-letter-spacing)] leading-[var(--h1-line-height)] [font-style:var(--h1-font-style)]">
+           WHO ARE WE?
+         </div>
+         <img
+           className="absolute w-[134px] h-[136px] top-[54px] left-[1035px] object-cover"
+           alt="img 1"
+           src="/images/uplogo.png" />
+         <img
+           className="w-[86px] h-[87px] top-[165px] left-[882px] absolute object-cover"
+           alt="img 2"
+           src="/images/firstlogo.png" />
+         <img
+           className="w-[107px] h-[48px] top-[89px] left-[92px]absolute object-cover"
+           alt="img 3"
+           src="/images/newlogo.png" />
+          <img
+            className=" w-[232px] h-[237px] top-[89px] left-[92px] object-cover"
+            alt="first LOGO"
+            src="/images/firstlogo.png" /> 
+       </div>
+     </div><div className="flex flex-col w-[1200px] items-start gap-[80px] relative bg-[#fff8f3]">
+         <div className="flex items-center justify-between self-stretch w-full relative flex-[0_0_auto]">
+           <div className="inline-flex flex-col items-start gap-[40px] relative flex-[0_0_auto]">
+             <div className="relative w-[694px] mt-[-1.00px] font-h2 font-[number:var(--h2-font-weight)] text-[#3e6eff] text-[length:var(--h2-font-size)] tracking-[var(--h2-letter-spacing)] leading-[var(--h2-line-height)] [font-style:var(--h2-font-style)] ">
+               OUR MISSION
+             </div>
+             <p className="relative w-[830px] font-text-1 font-[number:var(--text-1-font-weight)] text-[#293039] text-[length:var(--text-1-font-size)] tracking-[var(--text-1-letter-spacing)] leading-[var(--text-1-line-height)] [font-style:var(--text-1-font-style)]">
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+               ea commodo consequat.
+             </p>
+           </div>
+           <img className="relative w-[269px] h-[277.01px] object-cover" alt="img 4" src="/images/placeholder.png" />
+         </div>
+         <div className="flex items-center justify-between self-stretch w-full relative flex-[0_0_auto]">
+           <img
+             className="relative w-[269px] h-[277px] object-cover"
+             alt="img 5"
+             src="/images/placeholder.png" />
+           <div className="inline-flex flex-col items-end gap-[40px] relative flex-[0_0_auto]">
+             <div className="relative w-[694px] mt-[-1.00px] font-h2 font-[number:var(--h2-font-weight)] text-[#3e6eff] text-[length:var(--h2-font-size)] text-right tracking-[var(--h2-letter-spacing)] leading-[var(--h2-line-height)] [font-style:var(--h2-font-style)]">
+               HOW WE STARTED
+             </div>
+             <p className="relative w-[830px] font-text-1 font-[number:var(--text-1-font-weight)] text-[#293039] text-[length:var(--text-1-font-size)] text-right tracking-[var(--text-1-letter-spacing)] leading-[var(--text-1-line-height)] [font-style:var(--text-1-font-style)]">
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+               ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+               nulla pariatur.
+             </p>
+           </div>
+         </div>
+       </div></>
   );
-}
+};
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,44 +2,35 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      width: {
-        '128': '460px',
-      },
-      height: {
-        '128': '400px',
-      },
       colors: {
-        ais: {
-          black: '#1F1E1E',
-          'blue-gray': '#E1E8ED',
-          'light-blue': '#11BBFD',
-          'dark-blue': '#4565B8',
-          'light-gray': '#f5f5f5',
-          'dark-gray': '#444444',
-          'indigo': '#24314D',
-          'hover-indigo': '#374b75',
-          white: '#fafafa',
-          navy: '#202042',
-          gray: '#B8B8B8',
-        },
+        black: "var(--black)",
+        "variable-collection-bg": "var(--variable-collection-bg)",
+        "variable-collection-blue1": "var(--variable-collection-blue1)",
+        "variable-collection-blue2": "var(--variable-collection-blue2)",
+        "variable-collection-blue3": "var(--variable-collection-blue3)",
+        "variable-collection-dark-blue": "var(--variable-collection-dark-blue)",
+        "variable-collection-dark-blue1": "var(--variable-collection-dark-blue1)",
+        white: "var(--white)",
       },
-      scale: {
-        85: '0.85',
-        300: '3',
-        400: '4',
+      fontFamily: {
+        h1: "var(--h1-font-family)",
+        h2: "var(--h2-font-family)",
+        h3: "var(--h3-font-family)",
+        label: "var(--label-font-family)",
+        "text-1": "var(--text-1-font-family)",
+        "text-2": "var(--text-2-font-family)",
+        "text-small-link": "var(--text-small-link-font-family)",
       },
-      margin: {
-        73: '18.75rem',
+      boxShadow: {
+        large: "var(--large)",
+        medium: "var(--medium)",
+        small: "var(--small)",
+        xlarge: "var(--xlarge)",
+        xsmall: "var(--xsmall)",
+        xxlarge: "var(--xxlarge)",
+        xxsmall: "var(--xxsmall)",
       },
     },
-    fontFamily: {
-      sans: ['IBM Plex Sans', 'sans-serif'],
-      cairo: ['Cairo', 'sans-serif'],
-      hind: ['Hind Siliguri']
-    },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
