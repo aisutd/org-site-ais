e69b9d5ed405163b508de8c7bc3764b44e55ee1a
vvyn <vicavyn3@gmail.com>
2023-11-22 18:16:47 -0600

    change footer

--- components/common/SiteFooterAlt.tsx
+++ components/common/SiteFooterAlt.tsx
@@ -7,54 +7,17 @@ import IconButton from '@mui/material/IconButton';
 
 export default function SiteFooterAlt() {
   return (
-    <footer className="bg-ais-black text-white pt-5">
-      <div className="mx-auto max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-4xl 2xl:max-w-6xl flex justify-around gap-4 p-4 ">
-        <div className="grid grid-col-1 grid-flow-row place-items-center py-5 items-">
-          <img src="/ais_dark.svg" className="h-20" />
-          <div className="py-5">© Copyright 2021 AIS UTD, All Rights Reserved</div>
-        </div>
-        <div>
-          <div className="text-lg font-bold py-5">Sitemap</div>
-          <div>
-            <div className="block p-2">
-              <Link href="/">Home</Link>
-            </div>
-            <div className="block p-2">
-              <Link href="/events">Events</Link>
-            </div>
-            <div className="block p-2">
-              <Link href="/team">Team</Link>
-            </div>
-            <div className="block p-2">
-              <Link href="/about">About</Link>
-            </div>
-            <div className="block p-2">
-              <Link href="/join">Join</Link>
-            </div>
+    <footer className="bg-ais-black text-white pt-6">
+      <div className="border-white border-b-2 mx-auto max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-4xl 2xl:max-w-6xl flex justify-between py-4">
+        <div className="grid grid-col-1 grid-flow-row py-2">
+          <div className="flex gap-4 [cursor:pointer]">
+            <img src="images/ais_logo_white.png" className="h-[4rem] object-cover"/>
           </div>
         </div>
         <div>
-          <div className="text-lg font-bold py-5">Socials</div>
+          <img src="images/demystify-ai.png" className="mb-4 h-[1.8rem] object-cover ml-auto"/>
           <ul>
-            <div>
-              <div>
-                <IconButton
-                  onClick={() => {
-                    window.open('/dc', '_blank');
-                  }}
-                >
-                  <img src="/discord-mark-white.svg" className="h-5 fill-white" />
-                </IconButton>
-              </div>
-              <div>
-                <IconButton
-                  onClick={() => {
-                    window.open('/fb', '_blank');
-                  }}
-                >
-                  <FacebookOutlinedIcon className="h-6 text-white" />
-                </IconButton>
-              </div>
+            <div className='flex'>
               <div>
                 <IconButton
                   onClick={() => {
@@ -82,10 +45,20 @@ export default function SiteFooterAlt() {
                   <YouTubeIcon className="h-6 text-white" />
                 </IconButton>
               </div>
+              <div>
+                <IconButton
+                  onClick={() => {
+                    window.open('/dc', '_blank');
+                  }}
+                >
+                  <img src="/discord-mark-white.svg" className="h-5 fill-white" />
+                </IconButton>
+              </div>
             </div>
           </ul>
         </div>
       </div>
+      <div className="text-center pt-5 pb-10 text-xs">© Copyright 2021 AIS UTD, All Rights Reserved</div>
     </footer>
   );
 }
