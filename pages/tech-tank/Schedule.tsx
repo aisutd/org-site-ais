48c1d4539b6c787405fb698ab6ea0b6306b32bac
Mark Friant <mlf200000@utdallas.edu>
2024-04-01 12:32:37 -0500

    Updated tech tank schedule

--- pages/tech-tank/Schedule.tsx
+++ pages/tech-tank/Schedule.tsx
@@ -17,7 +17,7 @@ function Schedule() {
             >
               Click for Prompts (needs updating)
             </a>
-            April 13, 2024
+            April 13, 2024 @Sciences Building
           </p>
         </div>
         <div className="flex justify-center items-center">
@@ -26,18 +26,16 @@ function Schedule() {
             src="/tech-tank/skis.png"
           />
           <div className="text-wrap break-words px-[10%] w-[100%] md:px-[0%] md:w-[70%] md:pr-[5%] lg:w-[50%]">
-            <div className="pt-[10px]">8:30am:<pre className="inline">{'\t\t'}</pre>Check In + Sponsor Fair<br /></div>
-            <div className="pt-[10px]">9:00am:<pre className="inline">{'\t\t'}</pre>Kickoff Ceremony (SCI 1.220)<br /></div>
-            <div className="pt-[10px]">10:00am:<pre className="inline">{'\t'}</pre>Workshop A/Workshop B/Workshop C (SCI 1.210/2.230/2.235)<br /></div>
-            <div className="pt-[10px]">11:00am:<pre className="inline">{'\t'}</pre>AI Tech Panel/Speaker (SCI 1.220/2.230)<br /></div>
-            <div className="pt-[10px]">12:00pm:<pre className="inline">{'\t'}</pre>Makeathon Prompt Announcement (SCI 1.220)<br /></div>
-            <div className="pt-[10px]">12:30pm:<pre className="inline">{'\t'}</pre>Lunch (Blackstone Launchpad)<br /></div>
-            <div className="pt-[10px]">1:30pm:<pre className="inline">{'\t\t'}</pre>Makeathon Working Hours/Strategy Workshop/Build and Pitch Workshop (All SCI/SCI 2.230/SCI 2.235)<br /></div>
-            <div className="pt-[10px]">3:30pm:<pre className="inline">{'\t\t'}</pre>Sumbit Projects (SCI 1.220)<br /></div>
-            <div className="pt-[10px]">3:30pm:<pre className="inline">{'\t\t'}</pre>Networking (Blackstone Launchpad)<br /></div>
-            <div className="pt-[10px]">4:15pm:<pre className="inline">{'\t\t'}</pre>Presentations Start (SCI 1.220)<br /></div>
-            <div className="pt-[10px]">6:00pm:<pre className="inline">{'\t\t'}</pre>Judging Finalized (SCI 2.210)<br /></div>
-            <div className="pt-[10px]">6:30pm:<pre className="inline">{'\t\t'}</pre>Results/Closing (SCI 1.220)<br /></div>
+            <div className="pt-[10px]">9:00am:<pre className="inline">{'\t\t'}</pre>Officer Check In<br /></div>
+            <div className="pt-[10px]">10:00am:<pre className="inline">{'\t'}</pre>Attendee Check In<br /></div>
+            <div className="pt-[10px]">10:30am:<pre className="inline">{'\t'}</pre>Opening Ceremony<br /></div>
+            <div className="pt-[10px]">11:15am:<pre className="inline">{'\t'}</pre>Workshop Session 1<br /></div>
+            <div className="pt-[10px]">12:00pm:<pre className="inline">{'\t'}</pre>Workshop Session 2<br /></div>
+            <div className="pt-[10px]">1:00pm:<pre className="inline">{'\t\t'}</pre>Lunch<br /></div>
+            <div className="pt-[10px]">2:00pm:<pre className="inline">{'\t\t'}</pre>Prompt Announcement/Build + Pitch Working Hours<br /></div>
+            <div className="pt-[10px]">3:30pm:<pre className="inline">{'\t\t'}</pre>Judging<br /></div>
+            <div className="pt-[10px]">5:00pm:<pre className="inline">{'\t\t'}</pre>Intermission<br /></div>
+            <div className="pt-[10px]">5:30pm:<pre className="inline">{'\t\t'}</pre>Closing Ceremony/Winners Announced<br /></div>
           </div>
         </div>
       </div>
